eff23e7b05a6b2a96c20008d99ad95f918d88781
FengUyu <35463247+FengUyu@users.noreply.github.com>
2018-02-21 14:20:37 +0800

    Site updated: 2018-02-21 14:20:36

--- js/totop.js
+++ js/totop.js
@@ -1,22 +1,20 @@
-window.onload = function() {
-    var obn = document.getElementById('heart');
-    var timer = null;
-    var isTop = true;
-    window.onscroll = function() {
-        if (!isTop) {
-            clearInterval(timer);
-        }
-        isTop = false;
-    }
-    obn.onclick = function() {
-        timer = setInterval(function() {
-            var osTop = document.documentElement.scrollTop || document.body.scrollTop;
-            var ispeed = Math.floor(-osTop / 5);
-            document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
-            isTop = true;
-            if (osTop == 0) {
-                clearInterval(timer);
-            }
-        }, 30)
-    }
-}
+$(window).scroll(function() {
+    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+});
+
+$("#rocket").click(function() {
+    $("#rocket").addClass("launch");
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000, function() {
+        $("#rocket").removeClass("show launch");
+    });
+    return false;
+});
+
+$("#homelogo").click(function() {
+    $("html, body").animate({
+        scrollTop: $(window).height()
+    }, 1000, null);
+    return false;
+});
